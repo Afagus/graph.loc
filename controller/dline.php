3e758589f67d9ceeb15b93d960233a91169c080a
afagus <afagus.13@gmail.com>
2021-01-08 13:49:33 +0200

    between towns

--- controller/dline.php
+++ controller/dline.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'controller/listOfTowns.php';
+//mydebugger($listOfTowns);
 
 $x1 = 10;
 $x2 = 20;
@@ -8,10 +9,24 @@ $y1 = 50;
 $y2 = 70;
 
 
-function countOfLength($x1,$x2,$y1,$y2)
+function countOfLength($x1, $x2, $y1, $y2)
 {
-        return round(sqrt(pow(($x2 - $x1), 2) + pow(($y2 - $y1), 2)));
+    return round(sqrt(pow(($x2 - $x1), 2) + pow(($y2 - $y1), 2)));
 
 }
 
-echo countOfLength($x1,$x2,$y1,$y2);
+$way = '';
+
+foreach ($listOfTowns as $coordinates) {
+    foreach ($listOfTowns as $town) {
+//        if ($coordinates['id'] == $town['id']) {
+//            break;
+//        }
+            $way .= countOfLength($coordinates['coordinateX'], $town['coordinateX'], $coordinates['coordinateY'], $town['coordinateY']);
+            $way .= ' miles between:' . $town['name'] . '-' . $coordinates['name'] . '<br>';
+
+        }
+
+}
+countOfLength($x1, $x2, $y1, $y2);
+echo $way;
